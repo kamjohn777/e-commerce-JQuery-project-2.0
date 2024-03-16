50fd47cc1699bb296bbb433273393c1b9acabbd8
Johnson <kameron.johnson@hp.com>
2024-03-15 20:20:51 -0500

    added dependencies yaml and some styling starting to build UI

---
+++ test/app.test.js
@@ -0,0 +1,12 @@
+const { JSDOM } = require('jsdom');
+const jquery = require('jquery');
+const main = require('../main');
+
+const { window } = new JSDOM('<!doctype html><html><body><ul class="cart"></ul></body></html>');
+global.$ = jquery(window);
+
+test('addItemToCart adds an item to the cart', () => {
+  app.addItemToCart('Test item');
+  expect($('.cart').children().length).toBe(1);
+  expect($('.cart').children().first().text()).toBe('Test item');
+});
