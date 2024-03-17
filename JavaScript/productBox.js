d4a68a476b4db7c81d5fdeae4b92dcffce78b2cc
Johnson <kameron.johnson@hp.com>
2024-03-17 18:39:41 -0500

    now working on product box in js

---
+++ JavaScript/productBox.js
@@ -0,0 +1,26 @@
+// const productBoxTitles = [
+//     {title: 'New Arrivals'},
+//     {title: 'Best Sellers'},
+//     {title: 'Trending'},
+// ];
+
+const productBoxTitles = [
+    {
+        title: 'New Arrivals',
+        products: [
+            { name: 'Product 1', img: 'https://3.imimg.com/data3/OD/QM/MY-3132455/baby-shoes-250x250.jpeg', description: 'Description 1' },
+        ]
+    },
+    {
+        title: 'Best Sellers',
+        products: [
+            { name: 'Product 2', img: 'https://3.imimg.com/data3/OD/QM/MY-3132455/baby-shoes-250x250.jpeg', description: 'Description 2' },
+        ]
+    },
+    {
+        title: 'Trending',
+        products: [
+            { name: 'Product 3', img: 'https://3.imimg.com/data3/OD/QM/MY-3132455/baby-shoes-250x250.jpeg', description: 'Description 3' },
+        ]
+    },
+];
